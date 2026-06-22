853fac6bd95e682f7e895fa607c34b8b79966d7b
Cédric Martin <cedric.martin@open-groupe.com>
2026-06-22 11:16:06 +0200

    refactor: migrate from Jest to Vitest for testing framework
    - Updated package.json to remove Jest dependencies and add Vitest and related packages.
    - Removed MockRenderer.ts as it was no longer needed.
    - Added dmd.spec.ts to test Dmd class and its layers using Vitest.
    - Added options.spec.ts to test Options helper class functionality.
    - Created vitest.config.ts for Vitest configuration.
    - Added vitest.setup.ts to include vitest-canvas-mock setup.

---
+++ tests/dmd.spec.ts
@@ -0,0 +1,85 @@
+import {beforeEach, describe, expect, test, vi} from 'vitest';
+import {setupVitestCanvasMock} from 'vitest-canvas-mock';
+
+import {ChangeAlphaRenderer, DmdRenderer} from '../src/renderers';
+import {Dmd} from '../src'
+import {Options} from '../src/utils';
+import {AnimationLayer, CanvasLayer, SpritesLayer} from '../src/layers';
+import {DotShape} from "../src/enums";
+
+vi.mock('../src/renderers/dmdRenderer');
+vi.mock('../src/renderers/changeAlphaRenderer')
+
+describe('testing entry file', () => {
+
+    const canvas = document.createElement('canvas')
+
+    canvas.width = 1280
+    canvas.height = 390
+
+    beforeEach(() => {
+        vi.resetAllMocks();
+        setupVitestCanvasMock();
+
+        const mockInit = vi.fn()
+        DmdRenderer.prototype.init = mockInit
+        ChangeAlphaRenderer.prototype.init = mockInit
+
+        const mockRenderFrame = vi.fn()
+        DmdRenderer.prototype.renderFrame = mockRenderFrame
+        ChangeAlphaRenderer.prototype.renderFrame = mockRenderFrame
+    });
+
+
+    test('Class should be created', () => {
+        const dmd = new Dmd(canvas, 2, 1, 1, 1, DotShape.Square, 14, 1, true)
+        expect(dmd).toBeTruthy()
+        expect(dmd).toBeInstanceOf(Dmd)
+    });
+
+    test('Created Layer should exist and match class CanvasLayer', () => {
+
+
+        const dmd = new Dmd(canvas, 2, 1, 1, 1, DotShape.Square, 14, 1, true)
+
+        const layer = dmd.addCanvasLayer('test', {}, new Options(), {}, (l) => {
+            expect(l).toBe(layer)
+        }, () => {
+        })
+
+        expect(layer).toBeTruthy()
+        expect(layer instanceof CanvasLayer).toBe(true)
+    });
+
+    test('Created Layer should exist and match class AnimationLayer', () => {
+        const dmd = new Dmd(canvas, 2, 1, 1, 1, DotShape.Square, 14, 1, true)
+        const layer = dmd.addAnimationLayer('test', {}, new Options(), {}, (l) => {
+            expect(l).toBe(layer)
+        }, () => {
+        })
+
+        expect(layer).toBeTruthy()
+        expect(layer instanceof AnimationLayer).toBe(true)
+    });
+
+    test('Created Layer should exist and match class SpritesLayer', () => {
+        const dmd = new Dmd(canvas, 2, 1, 1, 1, DotShape.Square, 14, 1, true)
+        const layer = dmd.addSpritesLayer('test', {}, new Options(), {}, (l) => {
+            expect(l).toBe(layer)
+        }, () => {
+        })
+
+        expect(layer).toBeTruthy()
+        expect(layer instanceof SpritesLayer).toBe(true);
+    });
+
+    /*test('Created Layer should exist and match class TextLayer', () => {
+        const dmd = new Dmd(canvas, 2, 1, 1, 1, DotShape.Square, 14, 1, true)
+        const layer = dmd.addTextLayer('test', {}, new Options(), {}, (l) => {
+            expect(l).toBe(layer)
+        }, () => {})
+
+        expect(layer).toBeTruthy()
+        expect(layer instanceof TextLayer).toBe(true)
+    });*/
+});
