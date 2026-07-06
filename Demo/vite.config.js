7bd8fd33d782fa6b7da04c68de6e1188bb91bf34
Cédric Martin <cedmart1@gmail.com>
2026-07-06 09:35:15 +0200

    chore: update eslint and add globals dependency; refine lint command

--- Demo/vite.config.js
+++ Demo/vite.config.js
@@ -1,9 +1,14 @@
 import {defineConfig} from "vite";
 import {tscWatch} from "vite-plugin-tsc-watch";
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
     base: '/H5DMD/',
     plugins: [
-        tscWatch()
-    ]
+        tscWatch(),
+        mkcert()
+    ],
+    server: {
+        host: '0.0.0.0'
+    }
 });
---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  { ignores: ['**/dist/**'] },
+  js.configs.recommended,
+  tsPlugin.configs['flat/eslint-recommended'],
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        ...globals.browser,
+        GPUBufferUsage: 'readonly',
+        GPUTextureUsage: 'readonly',
+        GPUShaderStage: 'readonly',
+        GPUMapMode: 'readonly',
+        GPUColorWrite: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+  {
+    files: ['scripts/**/*.mjs'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+];
