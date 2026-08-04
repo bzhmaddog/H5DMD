038917810cb20083212b5815acc1f9af439f595e
Cédric Martin <cedric.martin@open-groupe.com>
2026-08-04 13:06:14 +0200

    3.0.2

--- src/dmd.ts
+++ src/dmd.ts
@@ -61,7 +61,7 @@ export class Dmd {
      * H5DMD library version. Single source of truth for the version string
      * (must be bumped together with package.json on release).
      */
-    static readonly version: string = '3.0.1'
+    static readonly version: string = '3.0.2'
 
     private _outputCanvas: HTMLCanvasElement
     private _layers: LayerDictionary
